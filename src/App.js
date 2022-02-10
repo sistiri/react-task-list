832fe19edc35814f2ef23c9be8aebb578fb682fa
sistiri <sistiri@yahoo.com>
2022-02-10 16:13:38 +0100

    Using the Custom Http Hook - useHttp()

--- src/App.js
+++ src/App.js
@@ -1,39 +1,57 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Tasks from './components/Tasks/Tasks';
-import NewTask from './components/NewTask/NewTask';
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./hooks/use-http";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://task-list-8b9d1-default-rtdb.europe-west1.firebasedatabase.app/tasks.json'
-      );
+  const transformTasks = (taskObj) => {
+    const loadedTasks = [];
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
+    for (const taskKey in taskObj) {
+      loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
     }
-    setIsLoading(false);
+
+    setTasks(loadedTasks);
   };
+
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp(
+    {
+      url: "https://task-list-8b9d1-default-rtdb.europe-west1.firebasedatabase.app/tasks.json",
+    },
+    transformTasks
+  );
+
+  // const fetchTasks = async (taskText) => {
+  //   setIsLoading(true);
+  //   setError(null);
+  //   try {
+  //     const response = await fetch(
+  //       "https://task-list-8b9d1-default-rtdb.europe-west1.firebasedatabase.app/tasks.json"
+  //     );
+
+  //     if (!response.ok) {
+  //       throw new Error("Request failed!");
+  //     }
+
+  //     const data = await response.json();
+
+  //     // const loadedTasks = [];
+
+  //     // for (const taskKey in data) {
+  //     //   loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+  //     // }
+
+  //     // setTasks(loadedTasks);
+  //   } catch (err) {
+  //     setError(err.message || "Something went wrong!");
+  //   }
+  //   setIsLoading(false);
+  // };
 
   useEffect(() => {
     fetchTasks();
