72e8e4a45168993140c1e0b616d10d2d5bd1751f
sistiri <sistiri@yahoo.com>
2022-02-10 15:51:55 +0100

    Building a Custom Http Hook - useHttp()

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -12,7 +12,7 @@ const NewTask = (props) => {
     setError(null);
     try {
       const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json',
+        'https://task-list-8b9d1-default-rtdb.europe-west1.firebasedatabase.app/tasks.json',
         {
           method: 'POST',
           body: JSON.stringify({ text: taskText }),
